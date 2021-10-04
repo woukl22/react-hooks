b78d5ccf78933b449207958a81b8eee6f31435a3
JaeUk Lee <69896250+woukl22@users.noreply.github.com>
2021-10-04 23:59:33 +0900

    Learn example code of 'useTabs'

--- src/App.js
+++ src/App.js
@@ -3,32 +3,39 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value }
-    } = event;
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-    if (willUpdate) {
-      setValue(value);
-    }
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  }
+];
+
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
   };
-  return { value, onChange };
 };
 
-export default function App() {
-  const maxLen = (value) => value.length <= 10;
-  /* const maxLen = (value) => !value.includes("@"); */
-  const name = useInput("Mr.", maxLen);
+const App = () => {
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
-      <h1>Hello</h1>
-      {/* It can be use like <input placeholder="Name" {...name}/> */}
-      <input placeholder="Name" value={name.value} onChange={name.onChange} />
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
-}
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
