18dc1918913cbbcc658187a419c4cd6d17995331
JaeUk Lee <69896250+woukl22@users.noreply.github.com>
2021-10-04 23:29:03 +0900

    Learn react hooks 'useInput' and example code of use 'validator'

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 export default function App() {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const maxLen = (value) => value.length <= 10;
+  /* const maxLen = (value) => !value.includes("@"); */
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>incrementItem</button>
-      <button onClick={decrementItem}>decrementItem</button>
+      <h1>Hello</h1>
+      {/* It can be use like <input placeholder="Name" {...name}/> */}
+      <input placeholder="Name" value={name.value} onChange={name.onChange} />
     </div>
   );
 }
