2a68c94f81a9856508fc8a303cad6de1c8ba6990
JaeUk Lee <69896250+woukl22@users.noreply.github.com>
2021-10-05 22:50:22 +0900

    Learn react hooks 'useTitle'

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
 const App = () => {
-  const sayHello = () => console.log("hello");
-  const [number, setNumber] = useState(0);
-  const [anumber, setANumber] = useState(0);
-  useEffect(sayHello, [number]);
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
   return (
     <div className="App">
       <div>Hi</div>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setANumber(anumber + 1)}>{anumber}</button>
     </div>
   );
 };
