8e84229d81277b6b5e3c5c8f7b63eaf4a82b9507
JaeUk Lee <69896250+woukl22@users.noreply.github.com>
2021-10-06 22:52:57 +0900

    Learn react hooks 'useFullscreen' - 2

--- src/App.js
+++ src/App.js
@@ -4,19 +4,36 @@ import "./styles.css";
 
 const useFullscreen = (callback) => {
   const element = useRef();
+  const runCb = (isFull) => {
+    if (callback && typeof callback === "function") {
+      callback(isFull);
+    }
+  };
   const triggerFull = () => {
     if (element.current) {
-      element.current.requestFullscreen();
-      if (callback && typeof callback === "function") {
-        callback(true);
+      if (element.current.requestFullscreen) {
+        element.current.requestFullscreen();
+      } else if (element.current.mozRequestFullScreen) {
+        element.current.mozRequestFullScreen();
+      } else if (element.current.webkitRequestFullScreen) {
+        element.current.webkitRequestFullScreen();
+      } else if (element.current.msRequestFullScreen) {
+        element.current.msRequestFullScreen();
       }
+      runCb(true);
     }
   };
   const exitFull = () => {
-    document.exitFullscreen();
-    if (callback && typeof callback === "function") {
-      callback(false);
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullscreen) {
+      document.mozCancelFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
     }
+    runCb(false);
   };
   return { element, triggerFull, exitFull };
 };
