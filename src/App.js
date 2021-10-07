dccb016b1d18a66fac66a3c3b38b9bb86c71bc7d
JaeUk Lee <69896250+woukl22@users.noreply.github.com>
2021-10-07 09:49:31 +0900

    Learn react hooks 'useAxios'

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
-
-const useNotification = (title, options) => {
-  if (!("Notification" in window)) {
-    return;
-  }
-  const fireNotif = () => {
-    if (Notification.permission !== "granted") {
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          new Notification(title, options);
-        } else {
-          return;
-        }
-      });
-    } else {
-      new Notification(title, options);
-    }
-  };
-  return fireNotif;
-};
+import useAxios from "./useAxios";
 
 const App = () => {
-  const triggerNotif = useNotification("Can I steal your kimchi?", {
-    body: "I love kimchi don't you"
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://woukl22.github.io/react-js_movie-web-service/"
   });
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <button onClick={triggerNotif}>Hello</button>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
 };
